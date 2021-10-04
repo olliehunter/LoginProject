3b988f3d36c8061a8551ced220a33ec560f77803
Ollie Hunter <ollie@olliehunter.com>
2021-10-04 20:38:42 +0100

    Added React Router and setup routes with onclick functions for logo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 import LoginPage from './components/LoginPage'
 import StudentPage from './components/StudentPage'
 import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
-      <LoginPage />
-      <StudentPage />
-     
+      <Router>
+        <Switch>
+           <Route path="/students">
+            <StudentPage />
+          </Route>
+          <Route path="/">
+            <LoginPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,50 +1,47 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
+
 
 class LoginPage extends Component {
 
     constructor(props) {
         super(props)
+        this.state = {}
+        this.handleLogon = this.handleLogon.bind(this);
+    }
     
-        this.state = {
-             
-        }
+    handleLogon(e) {
+        e.preventDefault();
+        console.log('You Clicked Logon')
+        window.location.href='/students'
+        
     }
     
     render() {
         return (
         
-                <form>
-                    <div>
-                <h2>Login Page</h2>
+    <form>
+        <div>
+            <h2>Login Page</h2>
                 {/* <br></br><br></br> */}
-
                 <label for="uname"><b>Username</b></label>
-        <input type="text" 
-           placeholder="Enter Username" 
-           name="uname" required>
-        </input>
+                <input type="text" 
+                placeholder="Enter Username" 
+                name="uname" required>
+                </input>
+                <br></br>
 
+            <label for="psw"><b>Password</b></label>
+            <input type="password" 
+                placeholder="Enter Password" 
+                name="psw" required>
+            </input>
+            <br></br>
+        <button type="submit" onClick={this.handleLogon}>Login</button>
+        <label></label>
         <br></br>
-
-    <label for="psw"><b>Password</b></label>
-    <input type="password" 
-        placeholder="Enter Password" 
-        name="psw" required>
-        </input>
-
-        <br></br>
-
-    <button type="submit">Login</button>
-    <label></label>
-    <br></br>
-
-    <div><input type="checkbox" /> Remember Me</div>
-
-    
-                    </div>
-     
-    
-            </form>
+        <div><input type="checkbox" /> Remember Me</div>
+        </div>
+    </form>
         )
     }
 }
